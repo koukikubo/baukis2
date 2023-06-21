5cb0f467c222fb64d76754d8c637d6f4cf463713
koukikubo <117063128+koukikubo@users.noreply.github.com>
2023-06-21 18:24:54 +0900

    アドレス範囲の設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.web_console.whitelisted_ips = ["172.16.0.0/12"]
+  #172.16.0.0/12は、172.16.0.0から172.31.255.255までのアドレス範囲。
 end
