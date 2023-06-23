59b9d38139f222f7d26e91aeca844424a7c74451
koukikubo <117063128+koukikubo@users.noreply.github.com>
2023-06-23 20:25:12 +0900

    Revert "ルーティングの設定"
    This reverts commit feec64f3f2c1dce290c1f3e5d29c1e0fd6f76f73.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  namespace :staff do
-    root "top#index"
-  end
-
-  namespace :admin do
-    root "top#index"
-  end
-
-  namespace :customer do
-    root "top#index"
-  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
