509d104645a74e891a12c9ef6efc65f8f095d627
koukikubo <117063128+koukikubo@users.noreply.github.com>
2023-06-23 20:43:03 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   namespace :staff do
     root "top#index"
   end
@@ -11,4 +12,7 @@ Rails.application.routes.draw do
     root "top#index"
   end
   
+=======
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+>>>>>>> parent of 393b878 (Revert "Revert "ルーティングの設定"")
 end
