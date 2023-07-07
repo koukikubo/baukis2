2bf29174de00b143494127bef3b6e7f2625af6cf
koukikubo <117063128+koukikubo@users.noreply.github.com>
2023-07-07 19:02:16 +0900

    resourceメッソド

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
+    resource :account
   end
-
   namespace :admin do
     root "top#index"
     get "login" => "sessions#new", as: :login
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
     delete "session" => "sessions#destroy"
     resources :staff_members
   end
-
   namespace :customer do
     root "top#index"
   end
