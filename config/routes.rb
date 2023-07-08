e605b6a8fbf83c43e85da9a7e6520fb800125ac1
koukikubo <117063128+koukikubo@users.noreply.github.com>
2023-07-08 21:50:45 +0900

    ルーティングのテストコード

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,27 @@
 Rails.application.routes.draw do
-  constraints host: "baukis2.example.com" do
-    namespace :staff, path: "" do
+  config = Rails.application.config.baukis2
+  
+  constraints host: config[:staff][:host] do
+    namespace :staff, path: config[:staff][:path]  do
       root "top#index"
       get "login" => "sessions#new", as: :login
       resource :session, onry: [ :create, :destroy ]
       resource :account, except: [ :new, :create, :destroy ]
     end
   end
+  constraints host: config[:admin][:host] do
+    namespace :admin, path:config[:admin][:path] do
+      root "top#index"
+      get "login" => "sessions#new", as: :login
+      resource :session, only: [ :create, :destroy ]
+      resources :staff_members
+    end
+  end
+  constraints host: config[:customer][:host] do
+    namespace :customer, path:config[:customer][:path] do
+      root "top#index"
+    end
+  end
   
-  namespace :admin do
-    root "top#index"
-    get "login" => "sessions#new", as: :login
-    resource :session, only: [ :create, :destroy ]
-    resources :staff_members
-  end
-  namespace :customer do
-    root "top#index"
-  end
+  
 end
