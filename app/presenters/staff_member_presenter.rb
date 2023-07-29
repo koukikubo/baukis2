e0e9dc3ae845f6c742181da9b25c65843b35b2b0
koukikubo <117063128+koukikubo@users.noreply.github.com>
2023-07-29 20:50:21 +0900

    モデルプレゼンターの定義と利用

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -1,2 +1,10 @@
 class StaffMemberPresenter < ModelPresenter
+  #職員の停止フラグのOn /Offを再現する記号を返す。
+  #On: BALLOT BOX WITH CHECK(U+2611)
+  #Off: BALOTT BOX (U+2610)
+  def suspended_mark
+    object.suspended? ?
+      view_context.raw("&#x2611;") :
+      view_context.raw("&#x2610;")
+  end
 end
